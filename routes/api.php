617c573cf30f27f1f2aecf879523e99a707789ee
anupama-au <anupamabalakrishnan555@gmail.com>
2026-09-14 12:31:13 +1000

    employee API routes corretion

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\AuthController;
 
-
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
