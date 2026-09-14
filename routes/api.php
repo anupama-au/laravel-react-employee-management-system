661e29b87b44748ae8dee95aaff4d9b2067439d7
anupama-au <anupamabalakrishnan555@gmail.com>
2026-09-14 12:18:09 +1000

    employee API routes are protected by auth:sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,11 @@
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\AuthController;
 
-Route::apiResource('employees', EmployeeController::class);
-
-Route::get('/employees/{id}', [EmployeeController::class, 'show']);
-Route::put('/employees/{id}', [EmployeeController::class, 'update']);
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('employees', EmployeeController::class);
+
     Route::apiResource('employees', EmployeeController::class);
 });
